b4d6ba2636c2cdfc359d0d6f39ec862c0a14c508
AHmedEMadEG <amoemo141996@gmail.com>
2024-09-11 17:24:43 +0300

    chore: added users and posts routes inside index.js for vercel deployment

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ require("express-async-errors");
 
 const errorHandler = require("./middlewares/errorHandler");
 
-const User = require("./models/user.model");
+// commenting them for vercel deployment
 
-const usersRoutes = require("./routes/users.routes");
-const postsRoutes = require("./routes/posts.routes");
-const commentsRoutes = require("./routes/comments.routes");
-const likesRoutes = require("./routes/likes.routes");
+// const usersRoutes = require("./routes/users.routes");
+// const postsRoutes = require("./routes/posts.routes");
+// const commentsRoutes = require("./routes/comments.routes");
+// const likesRoutes = require("./routes/likes.routes");
 
 const app = express();
 
@@ -27,19 +27,48 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
-app.use("/users", usersRoutes);
-app.use("/posts", postsRoutes);
-app.use("/comments", commentsRoutes);
-app.use("/likes", likesRoutes);
+// commenting them for vercel deployment
+
+// app.use("/users", usersRoutes);
+// app.use("/posts", postsRoutes);
+// app.use("/comments", commentsRoutes);
+// app.use("/likes", likesRoutes);
 
 app.use(errorHandler);
+
+// adding all routes here for vercel deployment
+
+/************************users routes****************************/
+const { signup, login } = require("./controllers/users.controllers");
+const auth = require("./middlewares/auth");
+const upload = require("./utils/multerConfig");
+
+app.post("/signup", upload.single('profilePicture'), signup);
+app.post("/login", login);
+
+/************************posts routes****************************/
+const {
+  createPost,
+  getPosts,
+  getPostById,
+  updatePost,
+  deletePost,
+} = require("./controllers/posts.controllers");
+
+app.post("/posts/create", auth, createPost);
+app.get("/posts/", getPosts);
+app.get("/posts/:id", getPostById);
+app.put("/posts/:id", auth, updatePost);
+app.delete("/posts/:id", auth, deletePost);
+
+module.exports = app;
+
+
 
 mongoose
   .connect(DB_URL)
   .then(async () => {
     console.log("Connected to DB");
-
-    app.listen(PORT, () => console.log(`server running on port ${PORT}`));
   })
   .catch((err) => {
     console.log(err.message);
